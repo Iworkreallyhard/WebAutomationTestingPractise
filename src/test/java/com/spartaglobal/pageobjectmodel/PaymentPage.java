4b0b361787c23e9c4893e90c5f2dce2650eb4282
trickaby <73993996+trickaby@users.noreply.github.com>
2021-02-04 16:08:19 +0000

    Payment Page interface implemented

--- src/test/java/com/spartaglobal/pageobjectmodel/PaymentPage.java
+++ src/test/java/com/spartaglobal/pageobjectmodel/PaymentPage.java
@@ -2,10 +2,12 @@ package com.spartaglobal.pageobjectmodel;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PaymentPage implements StandardProcedures{
 
     private final WebDriver webDriver;
+    By logo = By.cssSelector(".logo");
 
     public PaymentPage(WebDriver webDriver){
         this.webDriver = webDriver;
@@ -19,5 +21,24 @@ public class PaymentPage implements StandardProcedures{
     public ConfirmationPage clickPayByCheck(){
         webDriver.findElement(By.className("cheque")).click();
         return new ConfirmationPage(webDriver);
+    }
+
+    @Override
+    public WebElement selectCart() {
+        return webDriver.findElement(By.cssSelector("a[title*='shopping cart']"));
+    }
+    @Override
+    public WebElement selectLogo(){
+        return webDriver.findElement(logo);
+    }
+    @Override
+    public HomePage gotoHome(WebDriver webDriver) {
+        selectLogo().click();
+        return new HomePage(webDriver);
+    }
+    @Override
+    public CheckoutSummary gotoCart(WebDriver webDriver) {
+        selectCart().click();
+        return new CheckoutSummary(webDriver);
     }
 }
