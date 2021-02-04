a7ef4cac5d8020599e905d4592242968c133c483
Ahmed Abdul Rahman <a.rahman1198@gmail.com>
2021-02-04 16:49:02 +0000

    Update ItemPage.java

--- src/test/java/com/spartaglobal/pageobjectmodel/ItemPage.java
+++ src/test/java/com/spartaglobal/pageobjectmodel/ItemPage.java
@@ -1,32 +1,4 @@
 package com.spartaglobal.pageobjectmodel;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
 public class ItemPage implements StandardProcedures, ItemPopup {
-    WebDriver webDriver;
-    By logo = By.cssSelector(".logo");
-
-    @Override
-    public WebElement selectCart() {
-        return webDriver.findElement(By.cssSelector("a[title*='shopping cart']"));
-    }
-
-    @Override
-    public WebElement selectLogo() {
-        return webDriver.findElement(logo);
-    }
-
-    @Override
-    public HomePage gotoHome(WebDriver webDriver) {
-        selectLogo().click();
-        return new HomePage(webDriver);
-    }
-
-    @Override
-    public CheckoutSummary gotoCart(WebDriver webDriver) {
-        selectCart().click();
-        return new CheckoutSummary(webDriver);
-    }
 }
