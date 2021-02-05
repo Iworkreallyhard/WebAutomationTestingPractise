e39d09d50725e219381fe47350c1b15adbaefedd
Ahmed AbdulRahman <a.rahman1198@gmail.com>
2021-02-05 09:48:12 +0000

    fix orderConfirmationPage

--- src/test/java/com/spartaglobal/pageobjectmodel/OrderConfirmationPage.java
+++ src/test/java/com/spartaglobal/pageobjectmodel/OrderConfirmationPage.java
@@ -2,17 +2,42 @@ package com.spartaglobal.pageobjectmodel;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class OrderConfirmationPage implements StandardProcedures{
+public class OrderConfirmationPage implements StandardProcedures {
 
     private final WebDriver webDriver;
+    By logo = By.cssSelector("img[class*='logo']");
+    By cart = By.cssSelector("a[title*='shopping cart']");
 
     public OrderConfirmationPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
 
-    public OrderHistoryPage clickBackToOrders(){
+    public OrderHistoryPage clickBackToOrders() {
         webDriver.findElement(By.cssSelector("#center_column > p.cart_navigation.exclusive > a"));
         return new OrderHistoryPage(webDriver);
+    }
+
+    @Override
+    public WebElement selectCart() {
+        return webDriver.findElement(cart);
+    }
+
+    @Override
+    public WebElement selectLogo() {
+        return webDriver.findElement(logo);
+    }
+
+    @Override
+    public HomePage gotoHome(WebDriver webDriver) {
+        selectLogo().click();
+        return new HomePage(webDriver);
+    }
+
+    @Override
+    public CheckoutSummary gotoCart(WebDriver webDriver) {
+        selectCart().click();
+        return new CheckoutSummary(webDriver);
     }
 }
