855f0cef262873ba26d15c150d4507dbe27edcae
Andrei Pavel <samurahh@gmail.com>
2021-02-05 12:04:08 +0000

    Implemented missing functionality in ItemPage.java

--- src/test/java/com/spartaglobal/pageobjectmodel/ItemPage.java
+++ src/test/java/com/spartaglobal/pageobjectmodel/ItemPage.java
@@ -1,9 +1,14 @@
 package com.spartaglobal.pageobjectmodel;
 
+import com.spartaglobal.pageobjectmodel.enums.ProductColor;
+import com.spartaglobal.pageobjectmodel.enums.ProductSize;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
+import javax.swing.*;
 import java.util.concurrent.TimeUnit;
 
 public class ItemPage implements StandardProcedures, ItemPopup {
@@ -15,26 +20,29 @@ public class ItemPage implements StandardProcedures, ItemPopup {
     By continueButton = By.linkText("Continue shopping");
     By close = By.cssSelector("span[class='cross'][title*='close']");
 
-    private WebDriver webDriver;
+    private final WebDriver webDriver;
 
 
-    public ItemPage(WebDriver webDriver){
+    public ItemPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
-    
+
     @Override
     public WebElement selectCart() {
         return webDriver.findElement(cart);
     }
+
     @Override
-    public WebElement selectLogo(){
+    public WebElement selectLogo() {
         return webDriver.findElement(logo);
     }
+
     @Override
     public HomePage gotoHome(WebDriver webDriver) {
         selectLogo().click();
         return new HomePage(webDriver);
     }
+
     @Override
     public CheckoutSummary gotoCart(WebDriver webDriver) {
         selectCart().click();
@@ -45,7 +53,7 @@ public class ItemPage implements StandardProcedures, ItemPopup {
     @Override
     public WebElement selectCheckoutButton() {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        return  webDriver.findElement(checkout);
+        return webDriver.findElement(checkout);
     }
 
     @Override
@@ -75,10 +83,58 @@ public class ItemPage implements StandardProcedures, ItemPopup {
         selectContinueToShopping().click();
     }
 
+    public WebElement selectQuantity() {
+        return webDriver.findElement(By.cssSelector("p[id*='quantity'] input[type*='text'][id*='quantity_wanted']"));
+    }
 
+    public WebElement selectPlusQuantity() {
+        return webDriver.findElement(By.cssSelector("p[id*='quantity'] i[class*='plus']"));
+    }
 
+    public WebElement selectMinusQuantity() {
+        return webDriver.findElement(By.cssSelector("p[id*='quantity'] i[class*='minus']"));
+    }
 
+    public WebElement selectColor(ProductColor color) {
+        return webDriver.findElement(color.getSelector());
+    }
 
+    public WebElement selectSize(ProductSize productSize) {
+        return webDriver.findElement(productSize.getSelector());
+    }
 
+    public WebElement selectAddToCart() {
+        return webDriver.findElement(By.cssSelector("button[type='submit'][name='Submit']"));
+    }
 
+    public ItemPage changeQuantityTo(int quantity) {
+        new Actions(webDriver).doubleClick(selectQuantity()).sendKeys(Keys.BACK_SPACE, String.valueOf(quantity)).perform();
+        return this;
+    }
+
+    public ItemPage addQuantity() {
+        new Actions(webDriver).click(selectPlusQuantity()).perform();
+        return this;
+    }
+
+    public ItemPage minusQuantity() {
+        new Actions(webDriver).click(selectMinusQuantity()).perform();
+        return this;
+    }
+
+    public ItemPage select(ProductColor color) {
+        new Actions(webDriver).click(selectColor(color)).perform();
+        return this;
+    }
+
+    public ItemPage select(ProductSize productSize) {
+        new Actions(webDriver).moveToElement(webDriver.findElement(By.cssSelector("select[id='group_1']")))
+                .click(selectSize(productSize)).perform();
+        return this;
+    }
+
+    public ItemPage addToCart() {
+        new Actions(webDriver).click(selectAddToCart()).perform();
+        return this;
+    }
 }
